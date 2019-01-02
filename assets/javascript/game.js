748aa2c4fc3c463b5926eb200ce38119a7694c4f
chaosZeroFive <chaos.five.zero@gmail.com>
2019-01-02 16:27:01 -0600

    I Hate Javascript!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,127 @@
 // JavaScript Document
 
+$(".alert-warning").hide();
+$(".alert-success").hide();
+var randomNumber = parseInt(Math.floor(Math.random() * 121) + 19);
+    $("#game-number").html(randomNumber);
 
-//$(document).ready(function(){
-    //console.log("Ready!");
+var blueVal = parseInt(Math.floor(Math.random() * 13) + 1);
 
-//player clicks "let's Play"
-//generate all the random numbers using the same function with different parameters
-//generate random number between 19-120 for #game-number element
-var gameNum = randomNum(19, 120);
-//generate random number between 1-12 for #crystal-blue object
-var blueNum = randomNum(1, 20);
-var blue = parseInt(blueNum);
-    console.log("blueNum is not a number? " + isNaN(blueNum));
-//generate random number between 1-12 for #crystal-green object
-var greenNum = randomNum(1, 20);
-var green = parseInt(greenNum);
-    console.log("greenNum is not a number? " + isNaN(greenNum));
-//generate random number between 1-12 for #crystal-orange object
-var orangeNum = randomNum(1, 20);
-//generate random number between 1-12 for #crystal-purple object
-var purpleNum = randomNum(1, 20);
+var greenVal = parseInt(Math.floor(Math.random() * 13) + 1);
 
-var totalScore = 0;    
-    console.log("total is not a number? " + isNaN(totalScore));
-function randomNum(minVal, maxVal){
-    return Math.floor(Math.random()*(maxVal-minVal+1)+minVal);
+var orangeVal = parseInt(Math.floor(Math.random() * 13) + 1);
+
+var purpleVal = parseInt(Math.floor(Math.random() * 13) + 1);
+
+var score = 0;
+
+var wins = 0;
+
+var loss = 0;
+
+console.log("This is the value of randomNumber: " + randomNumber);
+console.log("This is the value of blueVal: " + blueVal);
+console.log("This is the value of greenVal: " + greenVal);
+console.log("This is the value of orangeVal: " + orangeVal);
+console.log("This is the value of purpleVal: " + purpleVal);
+console.log("This is the value of score: " + score);
+
+
+        $("#crystal-blue").click(function () {
+            if (score < randomNumber){
+                console.log("The Player's score before the click is " + score);
+                console.log("You Clicked Blue with a value of " + blueVal);
+                score += blueVal;
+                console.log("The players's score is now " + score);
+                $("#score").html(score);
+            }
+            if (randomNumber === score) {
+                gameWin();
+            }
+            if (score > randomNumber) {
+                gameLose();
+            }
+
+        });
+
+        $("#crystal-green").click(function () {
+            if (score < randomNumber){
+                console.log("The Player's score before the click is " + score);
+                console.log("You Clicked Green with a value of " + greenVal);
+                score += greenVal;
+                console.log("The players's score is now " + score);
+                $("#score").html(score);
+            }
+            if (randomNumber === score) {
+                gameWin();
+            }
+            if (score > randomNumber) {
+                gameLose();
+            }
+        });
+
+        $("#crystal-orange").click(function () {
+            if (score < randomNumber){
+                console.log("The Player's score before the click is " + score);
+                console.log("You Clicked Orange with a value of " + orangeVal);
+                score += orangeVal;
+                console.log("The players's score is now " + score);
+                $("#score").html(score);
+            }
+            if (randomNumber === score) {
+                gameWin();
+            }
+            if (score > randomNumber) {
+                gameLose();
+            }
+        });
+
+        $("#crystal-purple").click(function () {
+            if (score < randomNumber){
+                console.log("The Player's score before the click is " + score);
+                console.log("You Clicked Purple with a value of " + purpleVal);
+                score += purpleVal;
+                console.log("The players's score is now " + score);
+                $("#score").html(score);
+            }
+            if (randomNumber === score) {
+                gameWin();
+            }
+            if (score > randomNumber) {
+              gameLose();
+            }
+        });
+function gameWin() {
+        $(".alert-success").html("You Won!");
+        wins + 1;
+        $("#wins").html(wins);
+        $(".alert-success").show();
+        resetGame();
 }
 
-//check logic in console for #game-number element
-console.log("gameNum value: " + gameNum);
-//set html element text
-document.getElementById("game-number").innerHTML = gameNum;
-//check logic in console for #crystal-... object
-console.log("blueNum value: " + blueNum);
-console.log("greenNum value: " + greenNum);
-console.log("orangeNum value: " + orangeNum);
-console.log("purpleNum value: " + purpleNum);
-//log all numbers are generated
-console.log("All the numbers are generated!")
-
-//player clicks a crystal that will add a number to the player's score counter
-//----need a click event that adds the total of the crystals the player clicked
-function clickCrystal(a, b){
-    return a + b;
+function gameLose() {
+        $(".alert-warning").html("You Lost...");
+        loss + 1;
+        $("#wins").html(loss);
+        $(".alert-warning").show();
+        resetGame();
 }
-
-//to win the sum of the crystals clicked will match the random number
-
-//to lose the sum of the crystals clicked will be greater than the random number
-
-//win or loss counter is updated
-
-//game restarts on win or lose
-//----generate random number between 19-120 for #game-number object
-//----generate random number between 1-12 for #crystal-blue object
-//----generate random number between 1-12 for #crystal-green object
-//----generate random number between 1-12 for #crystal-orange object
-//----generate random number between 1-12 for #crystal-purple object
-//});
+function resetGame() {
+    $("#score").html(0);
+    $(".alert-warning").hide();
+    $(".alert-success").hide();
+    var randomNumber = parseInt(Math.floor(Math.random() * 121) + 19);
+    var blueVal = parseInt(Math.floor(Math.random() * 13) + 1);
+    var greenVal = parseInt(Math.floor(Math.random() * 13) + 1);
+    var orangeVal = parseInt(Math.floor(Math.random() * 13) + 1);
+    var purpleVal = parseInt(Math.floor(Math.random() * 13) + 1);
+    var score = 0;
+    $("#game-number").html(randomNumber);
+    $("#score").html(0);
+    console.log("This is the value of randomNumber: " + randomNumber);
+    console.log("This is the value of blueVal: " + blueVal);
+    console.log("This is the value of greenVal: " + greenVal);
+    console.log("This is the value of orangeVal: " + orangeVal);
+    console.log("This is the value of purpleVal: " + purpleVal);
+    console.log("This is the value of score: " + score);
+}
